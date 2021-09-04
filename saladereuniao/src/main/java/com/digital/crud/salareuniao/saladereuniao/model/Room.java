e64f81fb14d4d7a35e9df28b15c1da2e3553ed63
wolwerr <ricardo@dtmm.com.br>
2021-09-04 00:06:03 -0300

    Atualizando o código com lombok.

--- saladereuniao/src/main/java/com/digital/crud/salareuniao/saladereuniao/model/Room.java
+++ saladereuniao/src/main/java/com/digital/crud/salareuniao/saladereuniao/model/Room.java
@@ -1,6 +1,6 @@
 package com.digital.crud.salareuniao.saladereuniao.model;
 
-
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,72 +9,24 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
-
+@Data
 @Entity
-@Table(name="meetingroom")
+@Table(name = "meetingroom")
 public class Room {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    @Column(name="name", nullable = false)
     private String name;
+    @Column(name="date", nullable = false)
     private String date;
+    @Column(name="startHour", nullable = false)
     private String startHour;
+    @Column(name="endHour", nullable = false)
     private String endHour;
 
     public Room() {
 
-    }
-    public Room(long id, String name, String date, String startHour, String endHour){
-        this.id = id;
-        this.name = name;
-        this.date = date;
-        this.startHour = startHour;
-        this.endHour = endHour;
-    }
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    @Column(name="name", nullable = false)
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Column(name="date", nullable = false)
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    @Column(name="startHour", nullable = false)
-    public String getStartHour() {
-        return startHour;
-    }
-
-    public void setStartHour(String startHour) {
-        this.startHour = startHour;
-    }
-
-    @Column(name="endHour", nullable = false)
-    public String getEndHour() {
-        return endHour;
-    }
-
-    public void setEndHour(String endHour) {
-        this.endHour = endHour;
     }
 
     @Override
